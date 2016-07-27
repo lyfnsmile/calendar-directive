2fecbb86ffb9305c2ce0fea9224104671b367b6b
lyfnsmile <li.zhixiang@live.cn>
2016-07-27 19:54:04 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # calendar-directive
+
+### Usage
+
+- npm install
+- bower install
+- gulp server
+
+打开http://127.0.0.1:3000即可预览
+
+### More
+
+关于指令的详细开发过程请见:[http://fnpyud.com/2016/05/JavaScript%E8%84%9A%E6%9C%AC%E6%89%A7%E8%A1%8C%E9%A1%BA%E5%BA%8F%E5%88%86%E6%9E%90/](http://fnpyud.com/2016/05/JavaScript%E8%84%9A%E6%9C%AC%E6%89%A7%E8%A1%8C%E9%A1%BA%E5%BA%8F%E5%88%86%E6%9E%90/)
+
+### Feedback
+- *li.zhixiang@live.cn*
---
+++ src/app.js
@@ -0,0 +1,12 @@
+angular.module('myApp', ['calendarControl'])
+    .controller('myController', function($scope) {
+        $scope.selected = function(dt) {
+            console.log(dt);
+        };
+        $scope.config = {
+            monthSelection: true,
+            yearSelection: true,
+            navigation: true
+        };
+        $scope.date = (new Date().getMonth() + 1) + '/' + new Date().getDate() + '/' + new Date().getFullYear();
+    });
